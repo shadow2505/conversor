d0acdf5a74fe2d19a89a36fcdec28400a804a321
shadow2505 <141756242+shadow2505@users.noreply.github.com>
2023-08-22 10:04:34 -0600

    Add files via upload

--- conversor de temperatura.cpp
+++ conversor de temperatura.cpp
@@ -1,26 +1,38 @@
 #include <stdio.h>
+#include <stdlib.h>  // Necesario para la función system()
 
-int main() {
+int main()
+{
+    system("color 0C4");
     double temperature;
     int choice;
+    char repeat;
 
     const double celsiusToFahrenheitFactor = 9.0 / 5.0;
     const double fahrenheitToCelsiusFactor = 5.0 / 9.0;
     const double fahrenheitToCelsiusOffset = 32.0;
 
-    printf("Seleccione la conversión:\n1. Celsius a Fahrenheit\n2. Fahrenheit a Celsius\n");
-    scanf("%d", &choice);
+    do
+    {
+        printf("Seleccione la conversión:\n1. Celsius a Fahrenheit\n2. Fahrenheit a Celsius\n");
+        scanf("%d", &choice);
 
-    printf("Ingrese la temperatura: ");
-    scanf("%lf", &temperature);
+        printf("Ingrese la temperatura: ");
+        scanf("%lf", &temperature);
 
-    if (choice == 1)
-        printf("Temperatura en Fahrenheit: %.2lf °F\n", temperature * celsiusToFahrenheitFactor + fahrenheitToCelsiusOffset);
-    else if (choice == 2)
-        printf("Temperatura en Celsius: %.2lf °C\n", (temperature - fahrenheitToCelsiusOffset) * fahrenheitToCelsiusFactor);
-    else
-        printf("Opción no válida\n");
+        if (choice == 1)
+            printf("Temperatura en Fahrenheit: %.2lf °F\n", temperature * celsiusToFahrenheitFactor + fahrenheitToCelsiusOffset);
+        else if (choice == 2)
+            printf("Temperatura en Celsius: %.2lf °C\n", (temperature - fahrenheitToCelsiusOffset) * fahrenheitToCelsiusFactor);
+        else
+            printf("Opción no válida\n");
+
+        printf("¿Desea realizar otra conversión? (s/n): ");
+        scanf(" %c", &repeat);
+    } while (repeat == 's' || repeat == 'S');
 
     return 0;
 }
+
+
 
